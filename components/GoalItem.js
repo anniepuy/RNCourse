7aa8e67f66325801abfb57a4a30f347cc5995576
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-04-08 17:13:32 -0500

    visual affects for andriod

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,22 +1,25 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+
 function GoalItem(props) {
     return (
         <View style={styles.goalItem}>
-        <Text style={styles.goalText}>{props.text}</Text>
-      </View>
+            <Pressable android_ripple={{color: '#dddddd'}} onPress={props.onDeleteItem.bind(this, props.id)}>
+                <Text style={styles.goalText}>{props.text}</Text>
+            </Pressable>
+        </View>
     );
-};
+}
 
 export default GoalItem;
 
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
-        backgroundColor: '#5e08cc'
+        backgroundColor: '#5e0acc'
       },
       goalText: {
+        padding: 8,
         color: 'white'
       }
 });
